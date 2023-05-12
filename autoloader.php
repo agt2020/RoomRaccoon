245295b7229a26ec48535f3d0c7516eb111ea72a
AGT <agt2020@gmail.com>
2023-05-12 15:51:15 +0330

    initial commit

---
+++ autoloader.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'config.php';
+
+foreach (glob(__DIR__ . '/App/*.php') as $appFiles) {
+    include_once($appFiles);
+}
+
+foreach (glob(__DIR__ . '/Controllers/*.php') as $controllerFiles) {
+    include_once($controllerFiles);
+}
+
+foreach (glob(__DIR__ . '/Models/*.php') as $modelFiles) {
+    include_once($modelFiles);
+}
+
+
+
+?>
